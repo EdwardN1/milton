bd72b9aac4c92f0b454a60dd700a2978c0f8bab7
EdwardN1 <edward@technicks.com>
2019-07-21 07:51:29 +0100

    Install Boilerplate Theme

---
+++ assets/scripts/js/slick-waypoints-init.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function ($) {
+
+    $('[data-waypoint]').each(function () {
+        var $element = $(this);
+        var wpOffset = $element.data('waypoint-offset');
+        var wpDirection = $element.data('waypoint-direction');
+        var wpClass = $element.data('waypoint-class');
+
+        $element.waypoint(function (direction) {
+            if(direction==wpDirection) {
+                if(!$element.hasClass(wpClass)) {
+                    $element.addClass(wpClass);
+                }
+            }
+            this.destroy();
+        }, {
+            offset: wpOffset
+        });
+    });
+
+    $('[data-slick-slider]').slick();
+
+});
---
+++ parts/blocks/block-template.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Edward Nickerson
+ * Date: 05/01/2019
+ * Time: 14:42
+ */
