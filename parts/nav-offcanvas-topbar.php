1b11d6cf2faf8e3b126d0e79ca1b74ea8b9faf83
EdwardN1 <GH?841241ed>
2019-07-29 19:04:53 +0100

    Header and Footer Edits

--- parts/nav-offcanvas-topbar.php
+++ parts/nav-offcanvas-topbar.php
@@ -11,7 +11,7 @@
         <div class="grid-container">
             <div class="grid-x">
                 <div class="cell auto">
-					<?php $logo = get_field( 'logo', 'option' ) ?>
+                    <?php $logo = get_field('logo', 'option') ?>
                     <a href="/"><img class="logo" src="<?php echo $logo['url']; ?>"/></a>
                 </div>
                 <div class="cell shrink">
@@ -21,21 +21,31 @@
                             <div class="cell auto"></div>
                             <div id="mobile-cart-totals" class="cell shrink">
                                 <a href="#">
-                                    <img src="<?php echo get_icon( 'Basket' ); ?>"><div>Basket</div></a>
+                                    <img src="<?php echo get_icon('Basket'); ?>">
+                                    <div>Basket</div>
+                                </a>
                             </div>
-                            <div id="mobile-menu-button" class="cell shrink"><a data-toggle="off-canvas" class="text-center menu-button"><img
-                                            src="<?php echo get_icon( 'Menu Bars' ) ?>"><div>Menu</div></a></div>
+                            <div id="mobile-menu-button" class="cell shrink"><a data-toggle="off-canvas"
+                                                                                class="text-center menu-button"><img
+                                            src="<?php echo get_icon('Menu Bars') ?>">
+                                    <div>Menu</div>
+                                </a></div>
                         </div>
                     </div>
 
                     <div class="tel">
-                        <a href="<?php the_field( 'telephone_link', 'option' ); ?>"><?php the_field( 'telephone_number', 'option' ); ?></a>
+                        <a href="<?php the_field('telephone_link', 'option'); ?>"><?php the_field('telephone_number', 'option'); ?></a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
     <div id="site-nav">
-		<?php joints_top_nav(); ?>
+        <?php joints_top_nav(); ?>
+    </div>
+    <div id="mobile-search-bar" class="hide-for-large">
+        <div class="grid-container">
+            <?php get_search_form(); ?>
+        </div>
     </div>
 </div>
